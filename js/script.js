9cf7afae3dac54d5608284ed6ed8c7c6ac016b7a
AzzamHamdan <112507056+AzzamA1@users.noreply.github.com>
2023-03-03 04:11:32 -0600

    Added javascript files along with css beginning of mobile media

---
+++ js/script.js
@@ -0,0 +1,14 @@
+jQuery(function($){
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+}
+
+function jsalert(){
+  
+  var check = document.getElementByID("gpa");
+  
+  if(check.checked == true){
+    alert("You meet the GPA requirement");
+  }
+});
+
