20aba2aa86bfe189269e598f7780877c50f40fea
AzzamHamdan <112507056+AzzamA1@users.noreply.github.com>
2023-03-03 05:37:51 -0600

    Fixed errors

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 jQuery(function($){
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-}
+});
 
 function jsalert(){
-  
   var check = document.getElementByID("gpa");
   
   if(check.checked == true){
     alert("You meet the GPA requirement");
   }
-});
-
+};
