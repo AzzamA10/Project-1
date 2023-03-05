89eb4c3220b2324658094f57578a17e16dc0aec8
AzzamHamdan <112507056+AzzamA1@users.noreply.github.com>
2023-03-05 02:07:47 -0600

    Fixed javascript error, renamed submit button id

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-jQuery(function($){
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-});
-
-function jsalert(){
-  var check = document.getElementByID("gpa");
+function init(){
+  function jsalert(){
+    var check = document.getElementById("gpa");
   
-  if(check.checked == true){
-    alert("You meet the GPA requirement");
-  }
+    if(check.checked){
+      window.alert("You meet the GPA requirement, Welcome to Google!");
+    }
+	else
+	{
+	  window.alert("You do not meet the GPA requirement, good luck next year.");	
+	}
+  };
+  
+  var submitb = document.getElementById("submitt");
+  submitb.addEventListener("click", jsalert);
+  
 };
+window.addEventListener("load", init);
